443e704502deaa4decdef7347129de2977201b16
ridhoharinugroho <ridho.harinugroho@gmail.com>
2026-08-31 02:23:40 +0700

    feat: separate app reviews table sat set wat wet

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 /**
- * solosatset - Service Worker Engine v20260831_v82
+ * solosatset - Service Worker Engine v20260831_v83
  * Instant Cache Invalidation, Automatic Update & Clean Static Asset Loading
  */
 
-const CACHE_NAME = 'solosatset-cache-v20260831_v82';
+const CACHE_NAME = 'solosatset-cache-v20260831_v83';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
